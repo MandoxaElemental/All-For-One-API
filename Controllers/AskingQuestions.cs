8c2382a2f6d8b1bd5905e08a51ad5ecf9f47b1c8
MandoxaElemental <gprcards@gmail.com>
2025-01-13 15:07:22 -0800

    Finished Asking Questions

--- Controllers/AskingQuestions.cs
+++ Controllers/AskingQuestions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using All_For_One_API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace All_For_One_API.Controllers
@@ -10,6 +11,14 @@ namespace All_For_One_API.Controllers
     [Route("[controller]")]
     public class AskingQuestions : ControllerBase
     {
-        
+        private readonly AskingQuestionsServices _askingQuestionsServices;
+        public AskingQuestions(AskingQuestionsServices askingQuestionsServices){
+            _askingQuestionsServices = askingQuestionsServices;
+        }
+        [HttpPost]
+        [Route("AskingQuestions/{name}/{time}")]
+        public List<string> Question(string name, string time){
+            return _askingQuestionsServices.FinalQuestion(name, time);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<HelloWorldServices>();
+builder.Services.AddScoped<AskingQuestionsServices>();
 builder.Services.AddScoped<AddingTwoNumbersServices>();
 
 var app = builder.Build();
